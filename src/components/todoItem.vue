5988e94093118328b50c278b505376366d28cf29
chris <anyikamaduchris@gmail.com>
2022-09-15 14:01:58 -1100

    save

---
+++ src/components/todoItem.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="Todo" :class="{'iscompleted':todo.completed}">
+        <p>
+        <input type="checkbox" @click="markcomplete">
+        {{todo.title}}
+        <button >
+            X
+        </button>
+       </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "todoItem",
+    props:['todo'],
+    methods:{
+        markcomplete(){
+           console.log("i am working");
+           this.todo.completed = !this.todo.completed
+        }
+      
+    }
+}
+</script>
+
+<style scoped>
+.iscompleted{
+    text-decoration: line-through;
+}
+.Todo{
+    background: grey;
+    padding: 10px;
+    border-bottom: 1px dotted black;
+}
+.Todo p{
+    padding: 20px;
+    font-size: 30px;
+    color: black;
+   
+}
+input[type='checkbox']{
+    width: 20px;
+    height: 20px;
+}
+.Todo button{
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    outline: none;
+    color: white;
+    font-size: 25px;
+    float: right;
+    cursor: pointer;
+}
+</style>
--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,61 +1,23 @@
 <template>
-    <div class="Todo" v-for="todo in todos" :key="todo.id" :class="{'iscompleted':todo.completed}" >
-       <p>
-        <input type="checkbox" @click="markcomplete">
-        {{todo.title}}
-        <button>
-            X
-        </button>
-       </p>
-    
+    <div  v-for="todo in todos" :key="todo.id"  >
+    <todoItem :todo="todo"/>
     </div>
 </template>
 
 <script>
+import todoItem from './todoItem.vue';
+
 export default {
     name: "todoList",
-    props : ['todos'],
-    methods:{
-        markcomplete(){
-           console.log("i am working");
-           this.todo.completed = !this.todo.completed
-        }
-      
-    }
+    components:{
+    todoItem,
+
+},
+
+   
 }
 </script>
 
 <style scoped>
-.iscompleted{
-    text-decoration: line-through;
-}
-.Todo{
-    background: grey;
-    padding: 10px;
-}
-.Todo p{
-    padding: 20px;
-    font-size: 30px;
-    color: black;
-   
-}
-input[type='checkbox']{
-    width: 20px;
-    height: 20px;
-}
-.Todo button{
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    background: red;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: none;
-    outline: none;
-    color: white;
-    font-size: 25px;
-    float: right;
-    cursor: pointer;
-}
+
 </style>
