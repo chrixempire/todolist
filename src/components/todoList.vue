9e0b41285d8bbb407b61916af202ed4c4c32a93a
chris <anyikamaduchris@gmail.com>
2022-09-15 13:36:42 -1100

    new

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,14 +1,61 @@
 <template>
-    <h1>{{todo.title}}</h1>
+    <div class="Todo" v-for="todo in todos" :key="todo.id" :class="{'iscompleted':todo.completed}" >
+       <p :class="{'isco >
+        <input type="checkbox" @click="markcomplete">
+        {{todo.title}}
+        <button>
+            X
+        </button>
+       </p>
+    
+    </div>
 </template>
 
 <script>
 export default {
     name: "todoList",
-    props: ["todos"],
+    props : ['todos'],
+    methods:{
+        markcomplete(){
+           console.log("i am working");
+           this.todo.completed = !this.todo.completed
+        }
+      
+    }
 }
 </script>
 
 <style scoped>
-
+.iscompleted{
+    text-decoration: line-through;
+}
+.Todo{
+    background: grey;
+    padding: 10px;
+}
+.Todo p{
+    padding: 20px;
+    font-size: 30px;
+    color: black;
+   
+}
+input[type='checkbox']{
+    width: 20px;
+    height: 20px;
+}
+.Todo button{
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background: red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    outline: none;
+    color: white;
+    font-size: 25px;
+    float: right;
+    cursor: pointer;
+}
 </style>
